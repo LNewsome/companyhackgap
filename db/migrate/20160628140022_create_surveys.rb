c27ff3149021259a346de0841630136372299cf7
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-06-28 11:30:38 -0400

    Surveys file updated. No form.

--- db/migrate/20160628140022_create_surveys.rb
+++ db/migrate/20160628140022_create_surveys.rb
@@ -1,10 +1,6 @@
 class CreateSurveys < ActiveRecord::Migration
   def change
     create_table :surveys do |t|
-      t.string :title
-      t.test :description
-      t.string :company
-      t.string :url
 
       t.timestamps null: false
     end
